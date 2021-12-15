1eb17604a848ba160c7369903c01cbee80cdbd2d
mroik <mroik@delayed.space>
2021-12-15 17:23:03 +0100

    Checker improvement

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Constraints
 * Max runtime: 4s
 * Max memory size: 400MB
+
+# Dependencies
+checker.sh uses `valgrind` so make sure to install it using
+on Debian-based distros
+```sh
+sudo apt-get install valgrind
+```
+on Arch-based distros
+```sh
+sudo pacman -S valgrind
+```
--- checker.sh
+++ checker.sh
@@ -4,12 +4,26 @@ files=$(ls -b "$2" | grep ".in")
 for x in $files
 do
 	echo -n "Checking $x... "
-	p_out=$(./$1 < "$2"/"$x")
+	timeout 4 ./$1 < "$2"/"$x" 1>/dev/null
+	time_o=$?
+	if [[ $time_o = 124 ]]
+	then
+		echo "timeout"
+		break
+	fi
+	p_out=$(valgrind --log-file=/tmp/valgrind.out ./$1 < "$2"/"$x")
 	ris=$(cat "$2"/"$(echo $x | sed s/\.in/\.ans/)")
+	size=$(cat /tmp/valgrind.out | grep "total heap usage" | awk '{print $(NF-2)}' | sed s/,//g)
+	if (( $size > 419430400 ))
+	then
+		echo "memory exceeded: $size bytes used"
+		break
+	fi
 	if [ "$p_out" == "$ris" ]
 	then
 		echo "correct"
 	else
 		echo "wrong"
 	fi
+	rm /tmp/valgrind.out
 done
